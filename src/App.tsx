15082071265e0cca56fcc8e92b08c33124506ef1
yingying <18636abc@gmai.com>
2024-04-11 13:39:15 +0800

    setting: init and add tailwind, shadcn ui, react-router

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+import { Button } from "../src/components/ui/button.jsx";
+
+function App() {
+  return (
+    <>
+      <Button>jaja</Button>
+    </>
+  );
+}
+
+export default App;
