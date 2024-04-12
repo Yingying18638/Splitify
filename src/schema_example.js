e45ed6e0a507425e163c2518d2a4e230c8b6ff07
yingying <18636abc@gmai.com>
2024-04-12 16:26:55 +0800

    try: add schema_example.js

---
+++ src/schema_example.js
@@ -0,0 +1,61 @@
+import { group } from "console";
+
+const group = {
+  groupId: "",
+  users: [{ userId: "", name: "", email: "" }, {}, {}, {}],
+  expenses: [
+    {
+      date: "",
+      time: "",
+      item: "",
+      icon: "", // research
+      payersAndAmounts: [
+        {
+          name: "",
+          foreign_amount: 0,
+          tw_amount: 0,
+          currency: "",
+          exchange_rate: 0,
+        },
+      ],
+      participants: [{ name: "", customized_amount: 0 }],
+      note: "",
+      img: "", // research
+    },
+  ],
+  bill: { a: -100.0, b: 150.0, d: -250, c: 200 },
+  flow: [
+    {
+      from: "a",
+      to: "b",
+      amount: 100,
+    },
+    {
+      from: "a",
+      to: "c",
+      amount: 0,
+    },
+    {
+      from: "d",
+      to: "b",
+      amount: 50,
+    },
+    {
+      from: "d",
+      to: "c",
+      amount: 200,
+    },
+  ],
+  paidAmount: [{ from: "a", to: "b", amount: 10, time: "timestamp" }],
+  leftAmount: [{ from: "a", to: "b", amount: 90 }],
+  history: [group.expenses, paidAmount],
+};
+
+const user = {
+  userId: "",
+  name: "",
+  email: "",
+  img: "",
+  // password:'',
+  groupIds: [],
+};
