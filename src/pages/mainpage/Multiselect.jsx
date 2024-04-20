b827cc07bc323c3d39636f9cc5ac509b158529d7
yingying <18636abc@gmai.com>
2024-04-20 20:58:35 +0800

    fix: initialize select with all options by default, and give up resetting select when customizing

--- src/pages/mainpage/Multiselect.jsx
+++ src/pages/mainpage/Multiselect.jsx
@@ -1,42 +1,30 @@
 import React, { useEffect, useState } from "react";
 import { MultiSelect } from "react-multi-select-component";
-import { group, users } from "../../schema_example";
 import useStore from "../../utility/useStore";
-// const usersOptions = group?.users.map(({ name }) => {
-//   return { label: name, value: name };
-// });
-const options = group?.users.map(({ name }) => {
-  return { label: name, value: name };
-});
-// const options = [...usersOptions, { label: "其他", value: "其他" }];
 const Multiselect = () => {
-  const { newExpense, setNewExpense } = useStore();
+  const { newExpense, setNewExpense, group } = useStore();
   const { participants_customized } = newExpense;
-  const [selected, setSelected] = useState([]);
+  const options = group?.users?.map(({ name }) => {
+    return { label: name, value: name };
+  });
+  console.log(options);
+  const [selected, setSelected] = useState(options);
+
   useEffect(() => {
-    const newArr = selected.map((item) => item.value);
+    const newArr = selected?.map((item) => item.value);
     setNewExpense({
       ...newExpense,
       participants: newArr,
     });
   }, [selected]);
-  useEffect(() => {
-    setSelected([]);
-  }, [participants_customized]);
+  // useEffect(() => {
+  //   // setSelected([]);
+  // }, [participants_customized]);
   const customValueRenderer = (selected) => {
-    // const hasOther = selected.find((obj) =>
-    //   Object.values(obj).includes("其他")
-    // );
-    // const isAllSelected = selected.length === options.length;
-    // if (hasOther && !isAllSelected) return "其他";
-    // if (hasOther)
-    //   return selected.length
-    //     ? `平分（共${selected.length - 1}人）`
-    //     : "全部平分";
     const cusAmountArr = Object.values(participants_customized);
     const cusAmountTotal = cusAmountArr.reduce((acc, cur) => acc + cur, 0);
     if (cusAmountTotal > 0) return "自訂";
-    return selected.length ? `平分（共${selected.length}人）` : "全部平分";
+    return selected?.length ? `平分（共${selected?.length}人）` : "全部平分";
   };
   return (
     <>
