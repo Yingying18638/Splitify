b02b0cee9f4bf1dcc693588136db37bf553b09c2
yingying <18636abc@gmai.com>
2024-04-25 19:51:17 +0800

    fix: avatar layout

--- src/common_components/RootLayout.jsx
+++ src/common_components/RootLayout.jsx
@@ -13,12 +13,14 @@ const RootLayout = ({ children }) => {
   return (
     <div>
       <header className="bg-yellow-200  xl:h-16 h-12 fixed top-0 w-full">
-        <SignedOut>
-          <SignInButton></SignInButton>
-        </SignedOut>
-        <SignedIn>
-          <UserButton className="fixed z-[52] right-0"></UserButton>
-        </SignedIn>
+        <div className="fixed right-5 top-2">
+          <SignedOut>
+            <SignInButton></SignInButton>
+          </SignedOut>
+          <SignedIn>
+            <UserButton className="right-0"></UserButton>
+          </SignedIn>
+        </div>
       </header>
       {children === "signedIn" ? <Mainpage /> : <LandingPage />}
     </div>
