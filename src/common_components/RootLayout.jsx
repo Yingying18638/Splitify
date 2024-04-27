89b4e2041dcf38e68eca09df1b225042bde34181
yingying <18636abc@gmai.com>
2024-04-27 21:36:46 +0800

    feat: add logo

--- src/common_components/RootLayout.jsx
+++ src/common_components/RootLayout.jsx
@@ -1,5 +1,6 @@
-import React from "react";
-import { Outlet } from "react-router-dom";
+import React, { useState } from "react";
+import logo from "../assets/logo-no-background.svg";
+import { Menu } from "lucide-react";
 import {
   SignInButton,
   SignedIn,
@@ -10,10 +11,17 @@ import Mainpage from "../pages/mainpage";
 import LandingPage from "../pages/landingpage";
 
 const RootLayout = ({ children }) => {
+  const [isSideBarOpen, setIsSideBarOpen] = useState(false);
   return (
-    <div>
-      <header className="bg-yellow-200  xl:h-16 h-12 fixed top-0 w-full">
-        <div className="fixed right-5 top-2">
+    <>
+      <header className="bg-[#dda15e] h-16 fixed top-0 w-full flex items-center gap-10 justify-center">
+        <div className="fixed left-5 ">
+          <SignedIn>
+            <Menu onClick={() => setIsSideBarOpen(true)}></Menu>
+          </SignedIn>
+        </div>
+        <img src={logo} alt="logo" className="w-32  " />
+        <div className="fixed right-5 ">
           <SignedOut>
             <SignInButton></SignInButton>
           </SignedOut>
@@ -22,8 +30,17 @@ const RootLayout = ({ children }) => {
           </SignedIn>
         </div>
       </header>
-      {children === "signedIn" ? <Mainpage /> : <LandingPage />}
-    </div>
+      <div className="bg-[#fefae0] h-[100vh]">
+        {children === "signedIn" ? (
+          <Mainpage
+            isSideBarOpen={isSideBarOpen}
+            setIsSideBarOpen={setIsSideBarOpen}
+          />
+        ) : (
+          <LandingPage />
+        )}
+      </div>
+    </>
   );
 };
 
