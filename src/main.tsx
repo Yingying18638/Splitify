6fe0928f7c82ad66fee1ca61af42caf8f6452a34
yingying <18636abc@gmai.com>
2024-05-02 16:36:42 +0800

    fix: loading page

--- src/main.tsx
+++ src/main.tsx
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
       <ClerkLoading>
-        <div className="text-lg">Loading...</div>
+        <div className="text-md bg-[#fefae0] w-full h-[100vh]">Loading...</div>
       </ClerkLoading>
       <App />
     </ClerkProvider>
