ca303a3ab0f82fc96adb1af6c5cf9910ebd0ca0c
yingying <18636abc@gmai.com>
2024-05-13 14:25:47 +0800

    fix: loading layout, why moved?

--- src/main.tsx
+++ src/main.tsx
@@ -13,9 +13,11 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
       <ClerkLoading>
         
-        <div className="text-md bg-[#fefae0] w-full h-[100vh] flex justify-center pt-60"><Loading></Loading></div>
+        <div className="bg-[#fefae0] w-full h-[100vh] flex justify-center items-center "><Loading></Loading></div>
       </ClerkLoading>
       <App />
+      {/* <div className="bg-[#fefae0] w-full h-[100vh] flex  justify-center pt-[300px]"><Loading></Loading></div> */}
+
     </ClerkProvider>
   </React.StrictMode>
 );
