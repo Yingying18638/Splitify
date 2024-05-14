4a668448e4da38ab74fa4389a25d0bd71db47e60
yingying <18636abc@gmai.com>
2024-05-14 22:11:00 +0800

    fix: guide text

--- src/utility/useAddExpGuide.js
+++ src/utility/useAddExpGuide.js
@@ -3,6 +3,9 @@ import "driver.js/dist/driver.css";
 import useStore from "./hooks/useStore";
 const driverObj = driver({
   showProgress: true,
+  nextBtnText: '繼續',
+  prevBtnText: '返回',
+  doneBtnText:'完成',
   steps: [
     {
       element: "#whoPaid",
@@ -13,7 +16,7 @@ const driverObj = driver({
     },
     {
       element: "#evenPayer",
-      popover: { title: "一人付帳，直接選他", description: "就是這麼簡單" },
+      popover: { title: "若是一人付帳", description: "直接選擇付款人即可" },
     },
     {
       element: "#morePayerOptions",
@@ -28,7 +31,7 @@ const driverObj = driver({
     },
     {
       element: "#evenParticipated",
-      popover: { title: "若是平均分擔", description: "選擇須參與分擔的人就好" },
+      popover: { title: "若是平均分擔", description: "選擇須參與分擔的人即可" },
     },
     {
       element: "#cusParticipatedOptions",
