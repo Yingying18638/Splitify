05a48134abc1fd6d8bdb24897ff405647e2ad24f
yingying <18636abc@gmai.com>
2024-05-20 19:47:48 +0800

    improve: add second simpler algorithm

--- src/utility/calcFlow2.js
+++ src/utility/calcFlow2.js
@@ -1,4 +1,3 @@
-const totalBill = { a: -180.0, b: 150.0, d: -250, c: 200, e: 80 };
 function calcFlow(totalBill) {
   const entries = Object.entries(totalBill);
   const posEntries = entries.filter((entry) => entry[1] > 0);
@@ -9,58 +8,46 @@ function calcFlow(totalBill) {
   const sortedNegEntries = negEntries.sort(
     (a, b) => Math.abs(b[1]) - Math.abs(a[1])
   );
-  let lessLength = 0;
   const flows = [];
-  if (sortedPosEntries.length >= sortedNegEntries.length) {
-    lessLength = sortedNegEntries.length;
-  } else {
-    lessLength = sortedPosEntries.length;
-  }
+
   let sortedTempPosEntries = [...sortedPosEntries];
   let sortedTempNegEntries = [...sortedNegEntries];
+
   sortedTempPosEntries = sortedTempPosEntries.filter((item) => item[1] !== 0);
   sortedTempNegEntries = sortedTempNegEntries.filter((item) => item[1] !== 0);
-  console.log(flows, sortedTempPosEntries,sortedTempNegEntries);
+  while (sortedTempPosEntries.length && sortedTempNegEntries.length) {
+    const [posName, posAmount] = sortedTempPosEntries[0];
+    const [negName, negAmount] = sortedTempNegEntries[0];
 
-  while(sortedTempPosEntries.length>0 || sortedTempNegEntries.length>0){
-    for (let i = 0; i < lessLength; i++) {
-        if (sortedTempNegEntries[i][1] + sortedTempPosEntries[i][1] > 0) {
-          const flowObj = {
-            from: sortedTempNegEntries[i][0],
-            to: sortedTempPosEntries[i][0],
-            amount: Math.abs(sortedTempNegEntries[i][1]),
-          };
-          flows.push(flowObj);
-          sortedTempNegEntries = sortedTempNegEntries.filter(
-            (item) => item[0] !== sortedTempNegEntries[i][0]
-          );
-        } else if (sortedTempNegEntries[i][1] + sortedTempPosEntries[i][1] < 0) {
-          const flowObj = {
-            from: sortedTempNegEntries[i][0],
-            to: sortedTempPosEntries[i][0],
-            amount: sortedTempPosEntries[i][1],
-          };
-          flows.push(flowObj);
-          sortedTempPosEntries = sortedTempPosEntries.filter(
-            (item) => item[0] !== sortedTempPosEntries[i][0]
-          );
-        } else {
-          const flowObj = {
-            from: sortedTempNegEntries[i][0],
-            to: sortedTempPosEntries[i][0],
-            amount: sortedTempPosEntries[i][1],
-          };
-          flows.push(flowObj);
-          sortedTempPosEntries = sortedTempPosEntries.filter(
-            (item) => item[0] !== sortedTempPosEntries[i][0]
-          );
-          sortedTempNegEntries = sortedTempNegEntries.filter(
-            (item) => item[0] !== sortedTempNegEntries[i][0]
-          );
-        }
-      }
+    if (posAmount + negAmount > 0) {
+      const flowObj = {
+        from: negName,
+        to: posName,
+        amount: Math.abs(negAmount),
+      };
+      flows.push(flowObj);
+      sortedTempPosEntries[0][1]+=negAmount
+      sortedTempNegEntries.shift();
+    } else if (posAmount + negAmount < 0) {
+      const flowObj = {
+        from: negName,
+        to: posName,
+        amount: posAmount,
+      };
+      flows.push(flowObj);
+      sortedTempNegEntries[0][1]+=posAmount
+      sortedTempPosEntries.shift();
+    } else {
+      const flowObj = {
+        from: negName,
+        to: posName,
+        amount: posAmount,
+      };
+      flows.push(flowObj);
+      sortedTempPosEntries.shift();
+      sortedTempNegEntries.shift();
+    }
   }
-  console.log(flows, sortedTempPosEntries,sortedTempNegEntries);
+  return flows
 }
-calcFlow(totalBill);
 export default calcFlow;
--- src/utility/test/flow.test.js
+++ src/utility/test/flow.test.js
@@ -1,11 +1,11 @@
-import calcFlow from '../calcFlow'
+import calcFlow from "../calcFlow2";
 describe("flow", () => {
   test("calc totalBill { a: -100.0, b: 150.0, d: -250, c: 200 } to be", () => {
     expect(calcFlow({ a: -100.0, b: 150.0, d: -250, c: 200 })).toStrictEqual([
       {
-        from: "a",
-        to: "b",
-        amount: 100,
+        from: "d",
+        to: "c",
+        amount: 200,
       },
       {
         from: "d",
@@ -13,9 +13,9 @@ describe("flow", () => {
         amount: 50,
       },
       {
-        from: "d",
-        to: "c",
-        amount: 200,
+        from: "a",
+        to: "b",
+        amount: 100,
       },
     ]);
   });
